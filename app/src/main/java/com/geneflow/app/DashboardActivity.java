27a6ceb0280d3a1d04815aa4ff8139139d5f62bb
areejfatima0077 <fatimaareej056@gmail.com>
2026-06-22 11:52:50 +0500

    Adjusted logos showing diseases

--- app/src/main/java/com/geneflow/app/DashboardActivity.java
+++ app/src/main/java/com/geneflow/app/DashboardActivity.java
@@ -12,6 +12,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
